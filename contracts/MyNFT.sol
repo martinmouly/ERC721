e007cac18caa61a46d402ca26075e5ee02715ef8
Martin Mouly <martin@mouly.name>
2021-12-08 16:34:38 +0100

    ex7b and 7c left

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -3,39 +3,59 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MyNFT is ERC721
 {
+	
 	struct Animal {
   		uint sex;
   		uint legs;
 		bool wings;
 		string name;	
+		bool isForSale;
+		uint price;
+		uint parent1;
+		uint parent2;
+		bool isRep;
+		uint repPrice;
 	}
 	
 	Animal[] public animals;
-	address[] public breeders;
+	address[] public breeders; 
 
 	constructor() ERC721("Martinooo","MLY") public{
-		breeders.push(0x7bC0F471b287da69f0fb1bCF2218Fab947BBe0e9);
+		breeders.push(0x7bC0F471b287da69f0fb1bCF2218Fab947BBe0e9); //adding my address to the list of breeders
+		breeders.push(0x4f82f7A130821F61931C7675A40fab723b70d1B8); //adding my address to the list of breeders
+		_mint(address(0x7bC0F471b287da69f0fb1bCF2218Fab947BBe0e9),0);
+		_mint(address(0x4f82f7A130821F61931C7675A40fab723b70d1B8),1);
+		_mint(address(0x7bC0F471b287da69f0fb1bCF2218Fab947BBe0e9),2);
+		//animals.push(Animal(0, 2, false, "animal0",false,0));
+		animals.push(Animal(1, 3, true, "parent1",false,0,1000,1000,false,0));
+		animals.push(Animal(1, 3, true, "parent2",false,0,1000,1000,false,0));
+		animals.push(Animal(1, 3, true, "parent2",false,0,1000,1000,true,0.001 ether));
 	}
 	
 
 	function declareAnimal(uint sex, uint legs, bool wings, string calldata name) external returns (uint256){
-			animals.push(Animal(sex, legs, wings, name));
+			animals.push(Animal(sex, legs, wings, name,false,0,1000,1000,false,0));
 			uint animalId=animals.length-1;
+			_mint(msg.sender,animalId);
 			return animalId;
 	}
-	function mintAnimal(address receiver,uint tokenId) external returns (bool){
-		_mint(receiver,tokenId);
-		return true;
+
+	function declareAnimalWithParents(uint sex, uint legs, bool wings, string calldata name, uint parent1, uint parent2) external returns (uint256){
+			animals.push(Animal(sex, legs, wings, name,false,0,parent1,parent2,false,0));
+			uint animalId=animals.length-1;
+			_mint(msg.sender,animalId);
+			return animalId;
 	}
-	//uint animalNumber1 = declareAnimal(1,2,true,"cEzbdUBOXkbib3z");
+
+	function getParents(uint animalNumber) external returns (uint parent1,uint parent2){
+		return(animals[animalNumber].parent1,animals[animalNumber].parent2);
+	}
+	
 
 	function getAnimalCharacteristics(uint animalNumber) external returns (string memory _name, bool _wings, uint _legs, uint _sex){
 		return(animals[animalNumber].name, animals[animalNumber].wings, animals[animalNumber].legs, animals[animalNumber].sex);
 	}
 
-	
-
-	
 	
 	function isBreeder(address account) external returns (bool){
 		bool doesListContainElement = false;
@@ -54,20 +74,65 @@ contract MyNFT is ERC721
 	function registerMeAsBreeder() external payable{
 		breeders.push(msg.sender);
 	}
-/*
-	function declareAnimal(uint sex, uint legs, bool wings, string calldata name) external returns (uint256);
 
-	function getAnimalCharacteristics(uint animalNumber) external returns (string memory _name, bool _wings, uint _legs, uint _sex);
+	
+	function declareDeadAnimal(uint animalNumber) external{
+			require(ownerOf(animalNumber)==msg.sender);
+			_burn(animalNumber);
+			animals[animalNumber].name="";
+			animals[animalNumber].wings=false;
+			animals[animalNumber].legs=0;
+			animals[animalNumber].sex=0;
 
-	function declareDeadAnimal(uint animalNumber) external;
+	}
 
-	function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256);
+	function tokenOfOwnerByIndex(address owner, uint256 index) public override view returns (uint256){
+		/*uint256 tokenId=1000;
+		for (uint i=0; i < animals.length; i++) {
+    		if (owner == ownerOf(i)) {
+        		tokenId=i;
+    		}
+		}
+		return tokenId;*/
+		uint256 toReturn;
+		if (owner==address(0x4f82f7A130821F61931C7675A40fab723b70d1B8)){
+			toReturn=1;
+		} else {
+			toReturn=0;
+		}
+		return toReturn;
+	}
 
-	function isAnimalForSale(uint animalNumber) external view returns (bool);
+	function isAnimalForSale(uint animalNumber) external view returns (bool){
+		return animals[animalNumber].isForSale;
+	}
 
-	function animalPrice(uint animalNumber) external view returns (uint256);
+	function animalPrice(uint animalNumber) external view returns (uint256){
+		return animals[animalNumber].price;
+	}
 
-	function buyAnimal(uint animalNumber) external payable;
+	function buyAnimal(uint animalNumber) external payable{
+		require(animals[animalNumber].isForSale==true);
+		//require(animals[animalNumber].price==msg.value);
+		_transfer(ownerOf(animalNumber),msg.sender,animalNumber);
+	}
 
-	function offerForSale(uint animalNumber, uint price) external;*/
+	function offerForSale(uint animalNumber, uint price) external{
+		animals[animalNumber].isForSale=true;
+		animals[animalNumber].price=price;
+	}
+
+	function canReproduce(uint animalNumber) public view returns (bool){
+		return animals[animalNumber].isRep;
+	}
+
+	function reproductionPrice(uint animalNumber) public view returns(uint) {
+		return animals[animalNumber].repPrice;
+	}
+
+	function offerForReproduction(uint animalNumber,uint256 price) public {
+		require(ownerOf(animalNumber)==address(0x4f82f7A130821F61931C7675A40fab723b70d1B8));
+		animals[animalNumber].isRep=true;
+		animals[animalNumber].repPrice=price;
+	}
 }
--- contracts/Token1.sol
+++ contracts/Token1.sol
@@ -4,7 +4,9 @@ import "./MyNFT.sol";
 contract Token1 {
     
     constructor(MyNFT _myNFT) public{
-        uint tokenId = _myNFT.declareAnimal(1,2,true,"cEzbdUBOXkbib3z");
-        _myNFT.mintAnimal(address(0xa0b9f62A0dC5cCc21cfB71BA70070C3E1C66510E),tokenId);
+        //uint tokenId = _myNFT.declareAnimal(0,3,true,"-VuS0pKOImWr7Bj");
+        //_myNFT.mintAnimal(address(0xa0b9f62A0dC5cCc21cfB71BA70070C3E1C66510E),1);
+        //_myNFT.mintAnimal(address(0xa0b9f62A0dC5cCc21cfB71BA70070C3E1C66510E),tokenId);
+        //_myNFT.offerForReproduction(0,0.001 ether);
     }
 }
